2800603ecd9a5c255678be7b8763557fd05d0958
Rejane Rodrigues <sollanarejane@gmail.com>
2022-06-25 23:47:37 -0300

    house buy and rent

--- classes/house.py
+++ classes/house.py
@@ -1,18 +1,25 @@
 from classes.constants import Constants
-from classes.dice import Dice
 import random
 
-rent_percent = random.randint(50, 100) / 100
 
 class House:
-    def __init__(self, position):
-        self.position = position
+    def __init__(self):
         self.sale_cost = self.get_sale_cost()
         self.rent_cost = self.get_rent_cost()
         self.owner = None
     
     def get_sale_cost(self):
-        return Constants.INITIAL_MONEY / (Constants.HOUSES - (self.position - 1)) / Dice.tendency()
+        return random.randint(Constants.SALE_COST_MIN, Constants.SALE_COST_MAX)
 
     def get_rent_cost(self):
-        return self.sale_cost * rent_percent
+        return self.sale_cost * Constants.RENT_PERCENT
+    
+    def buy(self, owner):
+        self.owner = owner
+        owner.balance -= self.sale_cost
+    
+    def rent(self, owner):
+        owner.balance -= self.get_rent_cost
+        if (self.owner != None):
+            self.owner.balance += self.get_rent_cost
+        
--- classes/player.py
+++ classes/player.py
@@ -1,4 +1,54 @@
-class player:
-    def __init__(self):
-        self.balance = 300
+import random
+from classes.constants import Constants
 
+class Player:
+    def __init__(self, player_type):
+        self.balance = Constants.INITIAL_BALANCE
+        self.player_type = player_type
+        self.playing = True
+        self.position = 0
+    
+    def buy(self, house, decision):
+        if(decision):
+            house.buy(self)
+            return True
+        else:
+            house.rent(self)
+            return False
+
+class Cautious(Player):
+    def __init__(self):
+        super().__init__(Constants.PLAYER_CAUTIOUS)
+        
+    def buy(self, house):
+        decision = False
+        after_buy = self.balance - house.sale_cost
+        if(after_buy > Constants.PLAYER_CAUTIOUS_CONDITION):
+            decision = True
+        return super().buy(house, decision)
+
+class Demanding(Player):
+    def __init__(self):
+        super().__init__(Constants.PLAYER_DEMANDING)
+        
+    def buy(self, house):
+        decision = False
+        if(house.rent_cost > Constants.PLAYER_DEMANDING_CONDITION):
+            decision = True
+        return super().buy(house, decision)
+
+class Impulsive(Player):
+    def __init__(self):
+        super().__init__(Constants.PLAYER_IMPULSIVE)
+        
+    def buy(self, house):
+        decision = True
+        return super().buy(house, decision)
+
+class Random(Player):
+    def __init__(self):
+        super().__init__(Constants.PLAYER_RANDOM)
+
+    def buy(self, house):
+        decision = random.choice([True, False])
+        return super().buy(house, decision)
