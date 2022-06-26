361fb50a185060dfe90eb5444c52054d36e19e1c
Rejane Rodrigues <sollanarejane@gmail.com>
2022-06-26 00:28:28 -0300

    buy or rent

--- classes/game.py
+++ classes/game.py
@@ -1,33 +1,44 @@
 from classes.board import Board
 from classes.player import *
 from classes.dice import Dice
+import random
 
 class Game:
     def __init__(self):
         self.board = self.config_board()
         self.players = self.config_players()
         random.shuffle(self.players)
+        self.round()
 
     def config_board(self):
-        return Board()
-    
+        return Board().houses
+
     def config_players(self):
         return [Cautious(), Demanding(), Impulsive(), Random()]
     
     def round(self):
         for player in self.players:
-            new_position = player.position + (Dice.roll() - 1)
-            if(new_position >= Constants.HOUSES):
-                player.position = new_position - Constants.HOUSES
-            else:
-                player.position = new_position
-            self.check_buy(player)
-    
-    def check_buy(self, player):
-        house = self.board[player.position]
-        player.buy(house)
+            self.set_new_position(player)
+            self.check_player_balance(player)
+            print(player.type, player.balance)
 
-    
-    def check_players(self, player):
-        player.playing = False
+    def buy_or_rent(self, player):
+            house = self.board[player.position]
+            player.buy_or_rent(house)
+
+    def set_new_position(self, player):
+        new_position = player.position + Dice.roll()
+        if(new_position >= Constants.HOUSES):
+            player.position = new_position - Constants.HOUSES
+        else:
+            player.position = new_position
+
+    def check_player_balance(self, player):
+        if(player.balance < 0):
+            player.playing = False
         self.release_houses(player)
+
+    def release_houses(self, player):
+        for house in self.board:
+            if(house.owner == player):
+                house.owner = None
