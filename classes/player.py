33fd086252fbdb7746c4a27d2240966c27024659
Rejane Rodrigues <sollanarejane@gmail.com>
2022-06-26 00:27:18 -0300

    buy or rent

--- classes/player.py
+++ classes/player.py
@@ -2,13 +2,13 @@ import random
 from classes.constants import Constants
 
 class Player:
-    def __init__(self, player_type):
+    def __init__(self, type):
         self.balance = Constants.INITIAL_BALANCE
-        self.player_type = player_type
+        self.type = type
         self.playing = True
         self.position = 0
     
-    def buy(self, house, decision):
+    def buy_or_rent(self, house, decision):
         if(decision):
             house.buy(self)
             return True
@@ -20,35 +20,35 @@ class Cautious(Player):
     def __init__(self):
         super().__init__(Constants.PLAYER_CAUTIOUS)
         
-    def buy(self, house):
+    def buy_or_rent(self, house):
         decision = False
         after_buy = self.balance - house.sale_cost
         if(after_buy > Constants.PLAYER_CAUTIOUS_CONDITION):
             decision = True
-        return super().buy(house, decision)
+        return super().buy_or_rent(house, decision)
 
 class Demanding(Player):
     def __init__(self):
         super().__init__(Constants.PLAYER_DEMANDING)
         
-    def buy(self, house):
+    def buy_or_rent(self, house):
         decision = False
         if(house.rent_cost > Constants.PLAYER_DEMANDING_CONDITION):
             decision = True
-        return super().buy(house, decision)
+        return super().buy_or_rent(house, decision)
 
 class Impulsive(Player):
     def __init__(self):
         super().__init__(Constants.PLAYER_IMPULSIVE)
         
-    def buy(self, house):
+    def buy_or_rent(self, house):
         decision = True
-        return super().buy(house, decision)
+        return super().buy_or_rent(house, decision)
 
 class Random(Player):
     def __init__(self):
         super().__init__(Constants.PLAYER_RANDOM)
 
-    def buy(self, house):
+    def buy_or_rent(self, house):
         decision = random.choice([True, False])
-        return super().buy(house, decision)
+        return super().buy_or_rent(house, decision)
